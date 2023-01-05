6c97cf00c04ab7e380704fff2ae8aa1da7872898
Kumar810 <immohitpraja@gmail.com>
2023-01-05 18:07:19 +0530

    splash added by mohit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:visitantapp/core/allpages.dart';
-
 import 'core/routes.dart';
 
 
