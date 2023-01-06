e3c2f6e07d209192b3773b4b689d2bab6747883b
vipin yadav <yadav.vipin810@gmail.com>
2023-01-06 13:32:52 +0530

    forgot page updated

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      initialRoute: Routes.introduction,
+      initialRoute: Routes.forgot,
       getPages: allpage,
     );
   }
