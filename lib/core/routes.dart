f99011f83ae57f9c7b13f24c3ce87a96a09de7fe
Atul <rajakatul502@gmail.com>
2023-01-06 10:59:49 +0530

    login routes add

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -2,4 +2,5 @@ class Routes{
   static String admindashboard='/admindashboard_view.dart';
   static String otp='/otp_view.dart';
   static String introduction='/introduction_view.dart';
+  static String login='/introduction_view.dart';
 }
