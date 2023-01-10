5289d1c1b4b57a30baf8290f9e03433b1db439b3
vivek <vpatel5225@gmail.com>
2023-01-10 17:25:59 +0530

    welcome updated by vivek

--- lib/features/welcome/view/welcome_view.dart
+++ lib/features/welcome/view/welcome_view.dart
@@ -8,107 +8,84 @@ class WelcomeView extends GetView {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: Get.height,
-        color: Colors.white,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(height:Get.height*0.05,),
-             Padding(
-               padding: EdgeInsets.only(left: Get.width*0.05),
-               child: Text('Welcome',style: TextStyle(
-                fontSize:Get.height*0.04,
-                fontWeight: FontWeight.w500,
-                fontFamily: CustomFonts.alata,
-                color: Colors.black54
-
-            ),),
-             ),
-             SizedBox(height:Get.height*0.05,),
-            Image.asset('assets/images/welcome.png',height:Get.height*0.35,),
-            SizedBox(
-              child: Stack(
-                children: [
-                  Positioned(
-                    child: Container(
-                      width: Get.width,
-                      color: Colors.indigo,
-                      child: Container(
-                        margin: const EdgeInsets.all(15),
-                      height:Get.height*0.45,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(height:Get.height*0.1,),
-                            SizedBox(
-                              width: Get.width,
-                              child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.white,
-                                      shape: const StadiumBorder(),
-                                      elevation: 0),
-                                  onPressed: () {
-                                    Get.toNamed(Routes.login);
-                                  },
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(10),
-                                    child: Text(
-                                      'Login',
-                                      style: TextStyle(
-                                          color: Colors.indigo,
-                                          fontSize: Get.height*0.02,
-                                          fontWeight: FontWeight.w500,
-                                          fontFamily: CustomFonts.alata
-                                          ),
-                                    ),
-                                  )),
-                            ),
-                             SizedBox(height:Get.height*0.02,),
-                            SizedBox(
-                              width: Get.width,
-                              child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.white,
-                                      shape: const StadiumBorder(),
-                                      elevation: 0),
-                                  onPressed: () {
-                                    Get.toNamed(Routes.signup);
-                                  },
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(11),
-                                    child: Text(
-                                      'Signup for Admin',
-                                      style: TextStyle(
-                                          color: Colors.indigo,
-                                          fontSize:Get.height*0.02,
-                                          fontWeight: FontWeight.w500,
-                                          fontFamily: CustomFonts.alata),
-                                    ),
-                                  )),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: -5,
-                    child: SizedBox(
-                        width: Get.width,
-                        child: Image.asset(
-                          'assets/images/welcomewave.png',
-                          fit: BoxFit.fill,
-                        )),
-                  ),
-                ],
-              ),
-            )
-          ],
+    return Container(
+        decoration: BoxDecoration(
+          image:DecorationImage(image:AssetImage("assets/images/welcome2.png"),fit:BoxFit.cover),
         ),
-      ),
-    );
+       child: Scaffold(
+        backgroundColor: Colors.transparent,
+         body: Stack(
+            children: [
+              Positioned(
+                top: Get.height*.55,
+                  right:Get.width*0.47 ,
+                  child: Text("Welcome to",style: TextStyle(fontWeight: FontWeight.w800,fontSize:28,color: Colors.white,letterSpacing: 1),)),
+              Positioned(
+                  top: Get.height*.61,
+                  right:Get.width*0.51,
+                  child: Text("UBIVisit",style: TextStyle(fontWeight: FontWeight.w800,fontSize:28,color: Colors.white,letterSpacing: 4,fontFamily: CustomFonts.alata),)),
+              Positioned(
+                bottom: 100,
+                left:Get.width*0.1,
+                child:SizedBox(
+                  width: Get.width*.8,
+                  child: ElevatedButton(
+
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white
+                          ,
+                          shape: const StadiumBorder(),
+                          elevation: 0),
+                      onPressed: () {
+                        Get.toNamed(Routes.signup);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(11),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                              color: Colors.indigo,
+                              fontSize:Get.height*0.02,
+                              fontWeight: FontWeight.w800,
+                              fontFamily: CustomFonts.alata),
+                        ),
+                      )),
+                ),),
+
+
+              Positioned(
+                  bottom: 40,
+                  left:Get.width*0.1,
+                  child:SizedBox(
+                    width: Get.width*.8,
+                    child: ElevatedButton(
+
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white
+                            ,
+                            shape: const StadiumBorder(),
+                            elevation: 0),
+                        onPressed: () {
+                          Get.toNamed(Routes.signup);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(11),
+                          child: Text(
+                            'Signup for Admin',
+                            style: TextStyle(
+                                color: Colors.indigo,
+                                fontSize:Get.height*0.02,
+                                fontWeight: FontWeight.w800,
+                                fontFamily: CustomFonts.alata),
+                          ),
+                        )),
+                  ),)
+
+
+            ],
+       ),
+     ),
+
+       );
   }
 }
