18b6331cf63a24822ab402cefdb13c5df4e2820c
Kumar810 <immohitpraja@gmail.com>
2023-01-17 18:25:51 +0530

    login & signup completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:ubivisit/core/allpages.dart';
 import 'package:ubivisit/core/global/customfont.dart';
 import 'package:ubivisit/core/global/global.dart';
@@ -10,6 +12,8 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  var directory=await getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
