523a8d5e40139287099c9a2a89d9eb3f6268e2fd
mohitpraja <immohitpraja@gmail.com>
2023-02-08 15:11:03 +0530

    list seprated

--- lib/core/fbase/firebase.dart
+++ lib/core/fbase/firebase.dart
@@ -68,10 +68,12 @@ class FBase {
   static Stream collectionPathEmp = firestore
       .collection('ubivisit/ubivisit/users')
       .where('role', isEqualTo: 'employee')
+      .where('organization',isEqualTo:userInfo['organization'])
       .snapshots();
   static Stream collectionPathGuard = firestore
       .collection('ubivisit/ubivisit/users')
       .where('post', isEqualTo: 'Guard')
+      .where('organization',isEqualTo:userInfo['organization'])
       .snapshots();
   static bool isMatch = false;
   static RxMap userInfo = {}.obs;
